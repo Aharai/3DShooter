2c6304c7bf75f1d7f0a1cdc855bce3a975803666
Ponomarev Stanislav <freezak_36rus@mail.ru>
2020-04-07 14:53:46 +0300

    HW5

---
+++ Test/DestroyPoint.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+
+namespace Geekbrains
+{
+	public class DestroyPoint : MonoBehaviour
+	{
+	}
+}
---
+++ Test/TestExtension.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Geekbrains
+{
+    public sealed class TestExtension
+    {
+        private void NameMethod()
+        {
+            List<int> list = new List<int>();
+            List<int> list2 = new List<int>();
+            
+            
+            list.Add(5);
+            5.AddList(list).AddList(list2);
+
+            bool test = "true".TryBool();
+
+            var myClass = new MyClass{A = 7};
+            var c = myClass.DeepCopy();
+            c.A = 5;
+            Debug.Log(myClass.A);
+
+
+        }
+
+        class MyClass
+        {
+            public int A;
+        }
+    }
+}
